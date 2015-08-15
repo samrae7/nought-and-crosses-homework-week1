9e7c31f5e3d2f404b6a09658bab19463177ff35e
Sam Rae <samuelrae@gmail.com>
2015-08-15 01:05:34 +0100

    not sure if last commit worked

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -2,24 +2,24 @@ console.log('JS file is linked');
 
 var turn = 'x';
 
+function changeTurn() {
+  turn = turn === 'x' ? "o" : "x";
+}
+
+//grab all cells 
 var cells = document.getElementsByClassName('cell');
 
-[].forEach.call(cells, function(v,i,a) {
-v.addEventListener('click', function() {console.log('clicked');
-  this.innerHTML="<p>"+turn+"</p>";
-   
-    if (turn==='x') {
-      turn ='o';
-    } else {
-      turn='x';
-    };
 
+// for all cells add event listeners that will insert 'x' or 'o' when clicked, then change the turn so that it alternate.
+//NB: tried to separate out the function that gets called a(eg. takeTurn(){..}) but'this' needed to be called within here
+[].forEach.call(cells, function(v,i,a) {
+v.addEventListener('click', function() {
+  console.log('clicked');
+  this.innerHTML="<p>"+turn+"</p>";
+  changeTurn();
+  
   });
 });
 
-function takeTurn() {
-  
- 
-}
 
 
